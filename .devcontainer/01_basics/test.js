e56479e2be396e697838283d8f73602a06e230b7
Darshan R <156777489+DarshanCodify@users.noreply.github.com>
2024-07-01 14:09:25 +0000

    datatype

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -12,3 +12,25 @@
 // console.log(number);
 
 // const -> it can't reassign the values again
+
+// primitive datatype->number,string,null,boolean,undefined
+// const name = "darsh";
+// const age =18;
+// const brother = false;
+// const abc = null;
+// let a;
+// console.log(typeof a);
+
+//Reference datatype
+// const person ={
+//     firstname :"john",
+//     age : 30,
+// };
+// console.log(typeof person)
+
+// datatype -> type conversion from integer to the string and string to the numbers float etc and also parseint(tofixed)
+// type coehension
+const val1 = 5;
+const val2 = 6;
+const sum = val1+val2;
+console.log(sum);
