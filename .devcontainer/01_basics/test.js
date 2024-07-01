dc0de182cf91404c43eb7e253bb4774a5e3f5acd
Darshan R <156777489+DarshanCodify@users.noreply.github.com>
2024-07-01 04:39:33 +0000

    done with the variables

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("darshan")
+// console.log("darshan")
+
+// let learn the variables var,let,const
+// var-> this is es6 used upto 2015 currently not using 
+
+// var name = "darshan";
+//  console.log(name);
+// var name ="akash";
+// console.log(name);
+
+// let number = 123;
+// console.log(number);
+
+// const -> it can't reassign the values again
