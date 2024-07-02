746f91c673e5575ec7600ffa412fccb59c0a07ee
Darshan R <156777489+DarshanCodify@users.noreply.github.com>
2024-07-02 05:44:01 +0000

    strings done

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -30,7 +30,28 @@
 
 // datatype -> type conversion from integer to the string and string to the numbers float etc and also parseint(tofixed)
 // type coehension
-const val1 = 5;
-const val2 = 6;
-const sum = val1+val2;
-console.log(sum);
+// const val1 = 5;
+// const val2 = 6;
+// const sum = val1+val2;
+// console.log(sum);
+
+// starting with the string and stringmethods->concat,escaping,length,indexof,substring,changecase,append,slice,split,includes
+
+// numbers and numbermethods
+const val1 = 100;
+const val2 = 50;
+let val = val1+val2;
+val =Math.PI;
+val =Math.ceil(2.4);
+val =Math.floor(Math.random()*20);
+console.log(val);
+
+// stings template->length of the string,pushing the element to the array->push in thee end of the aray
+// unshift->adding the elements tto the front the array.pop->removing the elements(end) in the array
+// shift->removing the elements(front)in the aray
+
+// slicing-> numbers.slice(1,2)removing the value of the array elements ,reverse the array,concating,sort the elements in the array
+// sorting the elements in the asc x-y; for the desc y-x;
+
+
+
